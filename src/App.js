d4f3365ca69bd96a05afa8196bc983db2dce57c3
TheTimTurner <71408843+TheTimTurner@users.noreply.github.com>
2021-04-20 20:18:04 -0400

    button

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+  function say_hi() {
+      var fname = document.getElementById('input_1').value;
+      var html = 'Encrypted <b>' + fname + '</b> ' ;
+
+      document.getElementById('result').innerHTML = html;
+
+  }
+
+
+window.onload=function(){
+  document.getElementById('encrypt_1').addEventListener('click', say_hi);
 }
+
+return(
+    <html>
+      <head>
+        <title>Cryptography Class</title>
+      </head>
+      <body>
+      Input: <input id="input_1"></input>
+
+      <button id="encrypt_1">Encrypt</button>
+      <div id="result"></div>
+      </body>
+  </html>
+  );
+
+}
+
+
 
 export default App;
